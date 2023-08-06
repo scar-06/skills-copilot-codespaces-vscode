a595f7a92993fb8d0c2b365c147254627e113246
scar-06 <140164549+scar-06@users.noreply.github.com>
2023-08-06 21:35:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    document.getElementById("memberSkills").innerHTML = memberSkillsText;
+    document.getElementById("memberSkills").value = memberSkillsValue;
+    document.getElementById("member").innerHTML = memberText;
+    document.getElementById("member").value = memberValue;
+}
